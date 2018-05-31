c2193f60e546f10528bb205a2a2c87e4eeed07db
Alexandr Kasikov <qsefthuken@gmail.com>
2018-05-31 18:15:25 +0700

    Update add.php

--- add.php
+++ add.php
@@ -2,16 +2,15 @@
 require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
 $user = new CUser;
 $arFields = Array(
-  "NAME"              => "Сергей",
+  "NAME"              => "Иван",
   "LAST_NAME"         => "Иванов",
-  "EMAIL"             => "ivanov@microsoft.com",
-  "LOGIN"             => "b0007",
+  "EMAIL"             => "ivanov@ivanov.ru",
+  "LOGIN"             => "b0006",
   "LID"               => "ru",
   "ACTIVE"            => "Y",
   "GROUP_ID"          => array(1),
   "PASSWORD"          => "13820003",
   "CONFIRM_PASSWORD"  => "13820003",
-  "PERSONAL_PHOTO"    => $arIMAGE
 );
 
 $ID = $user->Add($arFields);
